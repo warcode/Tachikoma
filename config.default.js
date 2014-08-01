4e6ef823542654208d734a3ad65b7c5c10f15335
warcode <warcode@deny.io>
2014-08-02 01:47:04 +0200

    Init xmpp bot

---
+++ config.default.js
@@ -0,0 +1,12 @@
+var config = {};
+
+config.xmpp = {};
+config.web = {};
+
+config.xmpp.jid = '';
+config.xmpp.password = '';
+config.xmpp.host = '';
+config.xmpp.port = '';
+config.web.port = 55955;
+
+module.exports = config;
---
+++ tachikoma.js
@@ -0,0 +1,42 @@
+var xmpp = require('simple-xmpp');
+var config = require('./config');
+
+xmpp.on('online', function(data) {
+    console.log('Connected with JID: ' + data.jid.user);
+    console.log('Yes, I\'m connected!');
+});
+
+xmpp.on('chat', function(from, message) {
+    xmpp.send(from, 'echo: ' + message);
+});
+
+xmpp.on('error', function(err) {
+    console.error(err);
+});
+
+xmpp.on('subscribe', function(from) {
+    if (from === 'a.friend@gmail.com') {
+        xmpp.acceptSubscription(from);
+    }
+});
+
+xmpp.on('groupchat', function(conference, from, message, stamp) {
+    if(message === '!test')
+    {
+        xmpp.send(conference, 'AI satellite is operational.', true);
+    }
+    //console.log('%s says %s on %s on %s at %s', from, message, conference, stamp.substr(0,9), stamp.substr(10));
+});
+
+xmpp.connect({
+    jid: config.xmpp.jid,
+    password: config.xmpp.password,
+    host: config.xmpp.host,
+    port: config.xmpp.port
+});
+
+xmpp.join(config.xmpp.channel);
+
+//xmpp.subscribe('your.friend@gmail.com');
+// check for incoming subscription requests
+xmpp.getRoster();
